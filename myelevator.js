ddae976ecf8bc4bb0cf51ef5d01cee08d00782d0
maulana <maulana.robert.mr@gmail.com>
2017-07-14 17:27:38 +0700

    increase elevator speed

--- myelevator.js
+++ myelevator.js
@@ -4,7 +4,7 @@
  * Simulator.get_instance().get_requests()
  * Array of integers representing floors where there are people calling the elevator
  * eg: [7,3,2] // There are 3 people waiting for the elevator at floor 7,3, and 2, in that order
- * 
+ *
  * 2. Elevator object
  * To get all elevators, Simulator.get_instance().get_building().get_elevator_system().get_elevators()
  * Array of Elevator objects.
@@ -20,7 +20,7 @@
  * - Elevator people
  * elevator.get_people()
  * Array of people inside the elevator
- * 
+ *
  * 3. Person object
  * - Floor
  * person.get_floor()
@@ -30,16 +30,19 @@
  * person.get_wait_time_out_elevator()
  * - Get time waiting in an elevator
  * person.get_wait_time_in_elevator()
- * 
+ *
  * 4. Time counter
  * Simulator.get_instance().get_time_counter()
  * An integer increasing by 1 on every simulation iteration
- * 
+ *
  * 5. Building
  * Simulator.get_instance().get_building()
  * - Number of floors
  * building.get_num_floors()
  */
+
+var newConfig = Config;
+newConfig.elevator_speed_m_per_s = 2
 
 Elevator.prototype.decide = function() {
     var simulator = Simulator.get_instance();
@@ -48,22 +51,22 @@ Elevator.prototype.decide = function() {
     var elevators = Simulator.get_instance().get_building().get_elevator_system().get_elevators();
     var time_counter = simulator.get_time_counter();
     var requests = simulator.get_requests();
-    
+
     var elevator = this;
     var people = this.get_people();
     var person = people.length > 0 ? people[0] : undefined;
-    
+
     if(elevator) {
         elevator.at_floor();
         elevator.get_destination_floor();
         elevator.get_position();
     }
-    
+
     if(person) {
         person.get_floor();
         return this.commit_decision(person.get_destination_floor());
     }
-    
+
     for(var i = 0;i < requests.length;i++) {
         var handled = false;
         for(var j = 0;j < elevators.length;j++) {
